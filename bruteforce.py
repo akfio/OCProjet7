c495d45da080b97247fc53808b793fc0c6d40c53
akfio <ambroise.caffiaux@gmail.com>
2021-06-12 21:29:33 +0200

    fonction mise à jour

--- bruteforce.py
+++ bruteforce.py
@@ -1,9 +1,8 @@
 from operator import itemgetter
-import time
 import pandas
 
-
 actions = []
+
 
 def get_csv_to_dict(fichier):
     doc = pandas.read_csv(fichier)
@@ -11,11 +10,13 @@ def get_csv_to_dict(fichier):
         actions.append((doc["nom"][i], doc["prix"][i], doc["profit"][i]))
 
 
-def get_value(somme, pourcentage):
+def get_value(somme, benef):
+    pourcentage = 1 + benef / 100
     capital = somme * pourcentage
     ratio = capital - somme
     return ratio
-    #return round(ratio, 1)
+    # return round(ratio, 2)
+
 
 def get_binaire():
     n = len(actions)
@@ -28,6 +29,7 @@ def get_binaire():
         binaire = bin(j)[2:]
         table_binaire.append(binaire)
     return table_binaire
+
 
 def get_combinaisons(fichier, prix, nombre_de_combinaisons):
     get_csv_to_dict(fichier)
@@ -53,44 +55,4 @@ def get_combinaisons(fichier, prix, nombre_de_combinaisons):
     return print(sorted(combinaisons_valides, key=itemgetter(2), reverse=True)[:nombre_de_combinaisons])
 
 
-#get_combinaisons("data.csv", 500, 1)
-
-
-
-def get_brut():
-    get_csv_to_dict("data.csv")
-    n = len(actions)
-    table_entiers = []
-    for i in range(2 ** n):
-        entier = i
-        table_entiers.append(entier)
-    table_binaire = []
-    for j in table_entiers:
-        binaire = bin(j)[2:]
-        table_binaire.append(binaire)
-    combinaisons = []
-    for k in table_binaire:
-        combins = '0' * (n - len(k)) + k
-        combinaisons.append(combins)
-    prix_max = 500
-    combinaisons_valides = []
-    for combi in combinaisons:
-        prix_combi = 0
-        value = 0
-        for l in range(n):
-            if combi[l] == '1':
-                prix_combi += actions[l][1]
-                value += get_value(actions[l][1], actions[l][2])
-        if prix_combi <= prix_max:
-            combinaisons_valides.append((combi, prix_combi, value))
-    return sorted(combinaisons_valides, key=itemgetter(2), reverse=True)[0]
-
-
-a = time.time()
-print(get_brut())
-#print(get_combinaisons(500, 1))
-b = time.time()
-print(b-a)
-
-
-
+get_combinaisons("data.csv", 500, 1)
