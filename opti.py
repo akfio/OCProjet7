8f7130496bf53d4a95f7f83192baf8818f4a8985
akfio <ambroise.caffiaux@gmail.com>
2021-06-19 02:30:08 +0200

    fonction optimized.py ajouté

--- opti.py
+++ opti.py
@@ -1,5 +1,5 @@
 import pandas
-from operator import itemgetter
+
 
 actions = []
 
@@ -8,48 +8,44 @@ def get_csv_to_dict(fichier):
     doc = pandas.read_csv(fichier)
     for i in range(len(doc)):
         if doc["price"][i] > 0:
-            prix = doc["price"][i]*100
-            benef = doc["profit"][i]*100
-            actions.append((doc["name"][i], int(prix), int(benef)))
+            prix = doc["price"][i] * 100
+            actions.append((doc["name"][i], int(prix), doc["profit"][i]))
+
 
 def get_value(somme, benef):
-    profit = benef/100
-    pourcentage = 1 + profit / 100
-    new = somme/100
-    capital = new * pourcentage
-    ratio = capital - new
+    pourcentage = 1 + benef / 100
+    capital = somme / 100 * pourcentage
+    ratio = capital - somme / 100
     new_ratio = ratio * 100
     return int(new_ratio)
 
 
-def get_optimized(prix, fichier):
-    pri = prix*100
+def get_optimized(new_price, fichier):
+    new_price = new_price * 100
     get_csv_to_dict(fichier)
-    nb_actions = len(actions)*100
-    tableau = [[0 for w in range(pri + 1)] for w in range(nb_actions + 1)]
+    nb_actions = len(actions)
+    tableau = [[0 for w in range(new_price + 1)] for w in range(nb_actions + 1)]
     for i in range(1, nb_actions + 1):
-        j = int(i / 100)
-        for x in range(1, pri + 1):
-            if actions[j - 1][1] <= x:
+        for x in range(1, new_price + 1):
+            if actions[i - 1][1] <= x:
                 tableau[i][x] = max(
-                    get_value(actions[j - 1][1], actions[j - 1][2]) + tableau[i - 1][x - actions[j - 1][1]],
+                    get_value(actions[i - 1][1], actions[i - 1][2]) + tableau[i - 1][x - actions[i - 1][1]],
                     tableau[i - 1][x])
             else:
                 tableau[i][x] = tableau[i - 1][x]
 
     actions_name = []
     total_price = 0
-    while prix >= 0 and nb_actions >= 0:
-        e = actions[len(actions) - 1]
-
-        if tableau[nb_actions][pri] == tableau[nb_actions - 1][pri - e[1]] + get_value(e[1], e[2]):
+    while new_price >= 0 and nb_actions >= 0:
+        e = actions[nb_actions - 1]
+        if tableau[nb_actions][new_price] == tableau[nb_actions - 1][new_price - e[1]] + get_value(e[1], e[2]):
             actions_name.append(e[0])
             total_price += e[1]
-            pri -= e[1]
+            new_price -= e[1]
 
         nb_actions -= 1
-    return print('Actions achetées: ' + str(actions_name), '|| Profit: ' + str(tableau[-1][-1]/100),
-                 '|| Prix: ' + str(total_price/100))
+    return print('Actions achetées: ' + str(actions_name), '|| Profit: ' + str(tableau[-1][-1] / 100),
+                 '|| Prix: ' + str(total_price / 100))
 
 
 get_optimized(500, "data.csv")
